b308691846c474834be739778bc3176f05307771
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-08-08 13:46:26 -0400

    edits to server and sql database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,2 @@
-INSERT INTO burgers (burger_name, devoured) VALUES (Bison Burger);
-INSERT INTO burgers (burger_name) VALUES (Mushroom Burger);
-INSERT INTO burgers (burger_name, devoured) VALUES ();
-INSERT INTO burgers (burger_name, devoured) VALUES ();
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", true);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
-var methodOverride = require("method-override");
-var bodyParser = require("body-parser");
+// var methodOverride = require("method-override");
+// var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
 
@@ -8,12 +8,15 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 //static directory & parsers
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //override with the POST using [?_method=DELETE]
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
 
 //handlebars 
 var exphbs = require("express-handlebars");
@@ -22,8 +25,8 @@ app.set("view engine", "handlebars");
 
 
 //to import the routes
-var routes = require("./controllers/burgers_controller.js");
-app.use("/", routes);
+var routes = require("./controllers/burgers_controllers.js");
+app.use(routes);
 
 app.listen(PORT, function() {
     console.log("Listening PORT " + PORT);
